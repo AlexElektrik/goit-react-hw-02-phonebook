133b272f4e48aa02e953b695a7b1d2c50d7c03bb
Олександр <102046268+AlexElektrik@users.noreply.github.com>
2022-12-17 13:05:14 +0200

    виконав дз 2

---
+++ src/components/App.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1230px;
+  padding: 0 15px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
---
+++ src/components/ContactList/ContactItem/ContactItem.styled.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Item = styled.div`
+  font-size: 20px;
+  width: 480px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const DeleteButton = styled.button`
+  font-size: 14px;
+  margin-left: 20px;
+`;
